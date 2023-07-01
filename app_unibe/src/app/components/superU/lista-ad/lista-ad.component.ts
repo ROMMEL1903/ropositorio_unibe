6aef351d4e980967b3da96f0beeee8359387dcc8
jimmy jaramillo <crmada48@gmail.com>
2023-07-01 12:09:46 -0500

    Ceacion del componente login, super usuario

---
+++ app_unibe/src/app/components/superU/lista-ad/lista-ad.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-ad',
+  templateUrl: './lista-ad.component.html',
+  styleUrls: ['./lista-ad.component.scss']
+})
+export class ListaAdComponent {
+
+}
